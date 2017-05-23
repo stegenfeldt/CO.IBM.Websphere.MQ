c1c193f9552f36e3286c0ea51b4e7af3d6f1171e
stegenfeldt <samuel.tegenfeldt@atea.se>
2017-05-23 12:37:10 +0200

    New PS-script to gather queue perf data

---
+++ IBM.MQ/Scripts/Get-MQQueuePerfPB.ps1
@@ -0,0 +1,56 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$QueueName,
+    [Parameter(Mandatory=$true)]
+    [string]$QueueManager,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("CURDEPTH","IPPROCS","SESSIONS")]
+    [string]$DisplayParameter
+)
+
+#$cmdResult = @"
+#5724-H72 (C) Copyright IBM Corp. 1994, 2009.  ALL RIGHTS RESERVED.
+#Starting MQSC for queue manager EXT_QM.
+
+
+#     1 : Display Queue(   CHANGE_REQUEST) CURDEPTH
+#AMQ8409: Display Queue details.
+#   QUEUE(CHANGE_REQUEST)                   TYPE(QLOCAL)
+#   CURDEPTH(242)
+#One MQSC command read.
+#No commands have a syntax error.
+#All valid MQSC commands were processed.
+#"@
+
+function Extract-MQCurrDepthFromCmd ($CommandResult)
+{
+    $CommandResult -match "CURDEPTH\((.*)\)" | Out-Null
+    $currDepth = $matches[1] -as [double]
+    if ($currDepth) {
+        return $currDepth
+    } else {
+        return $null
+    }
+}
+
+function Invoke-MQDisplayQueue
+{
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$QueueName,
+        [Parameter(Mandatory=$true)]
+        [string]$QueueManager,
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("CURDEPTH","IPPROCS","SESSIONS")]
+        [string]$DisplayParameter
+    )
+    $commandString = "cmd"
+    $commandArguments = "/c echo Display Queue($QueueName) $DisplayParameter | runmqsc $QueueManager"
+
+    & $commandString $commandArguments
+}
+
+$mqQueueResult = Invoke-MQDisplayQueue -QueueName "TESTQUEUE" -QueueManager "TESTQUQUEMANAGER" -DisplayParameter CURDEPTH
+#Extract-MQCurrDepthFromCmd -CommandResult $cmdResult
