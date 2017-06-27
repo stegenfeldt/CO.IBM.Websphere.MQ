6f5b2a0cf062af7dd72bcc156f6cc292d251ad7f
stegenfeldt <samuel.tegenfeldt@atea.se>
2017-06-27 17:24:42 +0200

    Fixed powershel probe/perfcollection issues. https://nocentdocent.wordpress.com/2012/03/30/powershell-module-doesnt-obey-optional-parameters/

--- IBM.MQ/Scripts/Get-MQQueuePerfPB.ps1
+++ IBM.MQ/Scripts/Get-MQQueuePerfPB.ps1
@@ -2,21 +2,22 @@
 Param (
 	[Parameter(Mandatory=$true)]
 	[string] $QueueManager,
-	[string] $QueueName,
-	[Parameter(Mandatory=$true)]
-	[ValidateSet("CURDEPTH","IPPROCS","SESSIONS","OPPROCS")]
+	#[string] $QueueName,
 	[string] $DisplayParameter
 )
 
-function Extract-MQPerfdataFromCmd ($CommandResult, $DisplayParameter)
+function Get-MQPerfdataFromCmd ($CommandResult, $DisplayParameter)
 {
 	$CommandResult -match "$DisplayParameter\((.*)\)" | Out-Null
-	$perfResult = $matches[1] -as [double]
-	if ($perfResult) {
-		return $perfResult
-	} else {
-		return $null
-	}
+	if ($matches -ne $null) {
+	if ($matches.count -gt 1) {
+		$perfResult = $matches[1] -as [double]
+		if ($perfResult) {
+			return $perfResult
+		} else {
+			return $null
+		}
+	}}
 }
 
 function Invoke-MQDisplayQueue
@@ -45,25 +46,40 @@ function Get-MQQueueNames
 		[Parameter(Mandatory=$true)]
 		[string]$QueueManager
 	)
-
 	$commandString = "cmd"
 	$commandArguments = "/c echo Display QL(*) | runmqsc $QueueManager"
 	[string] $qlResult = & $commandString $commandArguments
 
-	$queueSearch = Select-String -InputObject $qlResult -Pattern "QUEUE\((.*)\) " -AllMatches
+	$queueSearch = Select-String -InputObject $qlResult -Pattern "QUEUE\((.*?)\)" -AllMatches
 	$queueNames = New-Object -TypeName System.Collections.ArrayList
-	foreach ($match in $queueSearch.Matches)
+	foreach ($match in $queueSearch.Matches.Groups)
 	{
-		[string]$queueName = $match.Groups[1].Value
-		$queueName = $queueName.Trim()
-		$queueNames.Add($queueName) | Out-Null
+		if ($match.Groups) {
+			[string]$queueName = $match.Groups[1].Value
+			$queueName = $queueName.Trim()
+			$queueNames.Add($queueName) | Out-Null
+		}
 	}
 
 	return $queueNames
 }
 
+function Write-DebugLog ($Message) {
+	if ($MyInvocation.ScriptName.Length -gt 0) {
+		$scriptName = $MyInvocation.ScriptName
+		$scriptName = $scriptName.Substring($scriptName.LastIndexOf("\")+1,$scriptName.LastIndexOf(".") - $scriptName.LastIndexOf("\")-1)
+	}
+	else {
+		$scriptName = $PID
+	}
+	$logFilePath = ("{0}\{2}_{1}.log" -f "C:\Windows\Temp",$(Get-Date -Format yyyyMMdd),$scriptName)
+	$Message = "{0}`t{1}`t{3}`t{2}" -f $(Get-Date -Format o),$PID,$Message,$env:USERNAME
+	Out-File -LiteralPath $logFilePath -InputObject $Message -Append
+}
+
 function Main()
 {
+	Write-DebugLog -Message "Starting script using parameters: QueueManager=$QueueManager; QueueName=$QueueName; DisplayParameter=$DisplayParameter"
 	# Is QueueName provided?
 	if ($QueueName)
 	{
@@ -82,19 +98,37 @@ function Main()
 
 	foreach ($queueName in $queueNames)
 	{
-		# execute "display <DisplayParameter>(<queueName>) | runmqsc <QueueManager>"
-		$displayResult = Invoke-MQDisplayQueue -QueueManager $QueueManager -QueueName $queueName -DisplayParameter $DisplayParameter
-		$perfData = Extract-MQPerfdataFromCmd -CommandResult $displayResult -DisplayParameter $DisplayParameter
+		Write-DebugLog -Message "Checking Queue: $queueName"
 
-		# Create property bag object and populate values
-		$omPb = $omApi.CreatePropertyBag()
-		$omPb.AddValue("Value", $perfData)
-		$omPb.AddValue("Object", $QueueManager)
-		$omPb.AddValue("Instance", $queueName)
-		$omPb.AddValue("Counter", $DisplayParameter)
 
-		# Return property bag to workflow
-		$omPb
+		# prepare array with relevant counters
+		$displayParameters = @(
+			"CURDEPTH",
+			"IPPROCS",
+			"OPPROCS",
+			"SESSIONS"
+		)
+		# loop through counters, return values in PB
+		foreach ($DisplayParameter in $displayParameters) {
+			# execute "display <DisplayParameter>(<queueName>) | runmqsc <QueueManager>"
+			$displayResult = Invoke-MQDisplayQueue -QueueManager $QueueManager -QueueName $queueName -DisplayParameter $DisplayParameter
+			# extract performance data
+			$perfData = Get-MQPerfdataFromCmd -CommandResult $displayResult -DisplayParameter $DisplayParameter
+
+			if ($perfData) {
+				# Create property bag object and populate values
+				$omPb = $omApi.CreatePropertyBag()
+				$omPb.AddValue("Value", $perfData)
+				$omPb.AddValue("Object", $QueueManager)
+				$omPb.AddValue("Instance", $queueName)
+				$omPb.AddValue("Counter", $DisplayParameter)
+
+				Write-DebugLog -Message "$DisplayParameter on $QueueManager/$queueName = $perfData"
+
+				# Return property bag to workflow
+				$omPb
+			}
+		}
 	}
 }
 Main
